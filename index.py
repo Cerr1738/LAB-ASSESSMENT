6e52a4f9c2b93aa7fae71919b6b4c54d5ec088cf
Eze Chisom <ezechisom966@gmail.com>
2026-06-30 21:01:55 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,13 @@
+angle1 = int(input("Enter first angle: "))
+angle2 = int(input("Enter second angle: "))
+
+# Calculate the third angle
+angle3 = 180 - (angle1 + angle2)
+
+# Check the triangle
+if angle3 <= 0:
+    print("Invalid triangle")
+elif angle1 == 90 or angle2 == 90 or angle3 == 90:
+    print("The triangle is a Right-Angled Triangle")
+else:
+    print("The triangle is NOT a Right-Angled Triangle")
